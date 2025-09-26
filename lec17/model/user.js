c82578487bb657ed2e8d23dc9e3a1785583856ab
Trishna <infinitydodo1001@gmail.com>
2025-09-26 10:03:15 +0530

    Added lec17 folder

---
+++ lec17/model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+const blog = require('./blog.js');
+
+const User = new Schema({
+    email: String,
+    username: String,
+    password: String,
+    blogs: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Blog'
+    }]
+});
+module.exports = mongoose.model('User', User);
